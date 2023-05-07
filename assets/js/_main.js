bb7d7ac64fe66946f413d34ea1cb1650bc0be4bb
Asungajinli <ssjj3552@gmail.com>
2023-05-08 01:37:17 +0900

    404

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -133,4 +133,24 @@ $(function() {
       $(this).append(anchor);
     }
   });
+
+  // 404 animation
+  var pageX = $(document).width();
+  var pageY = $(document).height();
+  var mouseY=0;
+  var mouseX=0;
+
+  $(document).mousemove(function( event ) {
+  //verticalAxis
+  mouseY = event.pageY;
+  yAxis = (pageY/2-mouseY)/pageY*300; 
+  //horizontalAxis
+  mouseX = event.pageX / -pageX;
+  xAxis = -mouseX * 100 - 100;
+
+  $('.box__ghost-eyes').css({ 'transform': 'translate('+ xAxis +'%,-'+ yAxis +'%)' }); 
+
+  //console.log('X: ' + xAxis);
+
+  });
 });
