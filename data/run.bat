3d476ab1824d4d52acea5d5ee70adc47e2276431
LEE SUNGJIN <ssjj3552@gmail.com>
2024-02-07 18:13:48 +0900

    v2.0.0 update

--- data/run.bat
+++
@@ -1,7 +0,0 @@
-jupyter nbconvert --to script ./football/scraping.ipynb
-jupyter nbconvert --to script ./esports/scraping.ipynb
-cd football
-python ./scraping.py
-cd ../esports
-python ./scraping.py
-cd ..
