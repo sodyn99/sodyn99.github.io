9b082e5808d76a0c826b2ccb0f439eeee0a8b311
LEE SUNGJIN <ssjj3552@gmail.com>
2024-02-23 22:36:36 +0900

    v2.0.1 update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,13 +2,13 @@ var store = [{
         "title": "Blog 소개",
         "excerpt":"여기 Blog 페이지에는 많은 것을 올리진 않을 것 같고 일상적인 것들이나 생각 같은 것들만 간단하게 쓸 것같다.  ","categories": ["daily"],
         "tags": [],
-        "url": "/daily/1/",
+        "url": "/daily/blog-%EC%86%8C%EA%B0%9C/",
         "teaser": null
       },{
         "title": "5G-Advanced 및 6G 동향",
         "excerpt":"5G 서비스는 과도기라는 말을 들을 정도로 어려움을 겪었다. 통신사들 자체의 서비스적인 측면을 떠나서 한국의 경우 이미 4G에 대한 만족도가 높은 상황에서 5G에 큰 메리트를 느끼기 어려웠다 라고 볼 수 있을 것 같다. 4G가 상용화 되면서 OTT나 Youtube와 같은 서비스들이 사용자들에게 새로운 경험을 선사했는데 5G는 아직까지도 대다수의 사람들에게 신선한 경험을 선사하는...","categories": ["6G"],
         "tags": [],
-        "url": "/6g/2/",
+        "url": "/6g/6G-%EB%8F%99%ED%96%A5/",
         "teaser": null
       },{
     "title": "5G 개요",
@@ -21,7 +21,7 @@ var store = [{
     "excerpt":"표준화 표준화 단체는 크게 2가지로 알면 된다. 바로 ITU와 3GPP이다. ITU는 국제전기통신연합으로 유엔 산하의 국제 기구다. 기술 표준을 제정하는 것 뿐만 아니라 주파수 관리, 위성 관리, 개발도상국 기술 지원 등의 역할을 하고 있다. 3GPP는 유럽의 ETSI, 일본의 TTC, 중국의 CCSA, 한국의 TTA, 미국의 ATIS가 연합하여 설립한 국제 표준화 단체이다. 3GPP의...","url": "http://localhost:4000/study/communication/5g/1/1/"
   },{
     "title": "6G 개요",
-    "excerpt":"#   — –&gt; &lt;!–  Reference     TTA, “3GPP, 6G 표준화 주요 일정 확정,” TTA 보도 자료, 2023년 12월 18일. [Online]. Available: https://www.tta.or.kr/tta/selectBbsNttView.do?key=76&amp;bbsNo=107&amp;nttNo=13137&amp;searchCtgry=&amp;searchCnd=all&amp;searchKrwd=&amp;integrDeptCode=&amp;pageIndex=1. [Accessed: 19- Feb- 2024].   ETSI  ","url": "http://localhost:4000/study/communication/6g/1/"
+    "excerpt":"#     ","url": "http://localhost:4000/study/communication/6g/1/"
   },{
     "title": "기본 철학",
     "excerpt":"1. 높은 주파수 대역 넓은 대역폭을 위해 mmWave를 사용. &#8618; 높은 대역 But! mmWave를 사용하면, pathloss &uarr; cell coverage &darr; 해결? MIMO, Beamforming, 낮은 주파수 대역과 협력 운용. 2. Ultra-Lean 'Always-on' 신호를 줄이기 위함. always-on? 사용자 트래픽에 상관없이 전송되는 신호(e.g. 기지국 감지 신호, 브로드캐스트 신호, 항상 켜져있는 레퍼런스 시그널 등)....","url": "http://localhost:4000/study/communication/5g/1/2/"
@@ -77,6 +77,9 @@ var store = [{
     "title": "Copyright Attribution",
     "excerpt":"Powered by Jekyll &amp; Minimal Mistakes. Source & License Logo Logo: 3D monstera plant with holes in its leaves by Icons 8 Source/License: Illustration by Icons 8 from Ouch! Fonts Font: Suit by Sunn Source/License: SIL Open Font License v1.1 Font: Roboto by Christian Robertson Source/License: Google Fonts Attribution Font:...","url": "http://localhost:4000/copyright/"
   },{
+    "title": "Data Analysis",
+    "excerpt":"","url": "http://localhost:4000/study/data_analysis/"
+  },{
     "title": "Bluetooth Generation",
     "excerpt":"Bluetooth 1 GFSK 변조 방식을 이용해 최대 1 Mbps, 실제로는 0.7 Mbps의 전송속도와 최대 10 m의 전파 범위를 지원한다. Bluetooth 2 Bluetooth 2.0은 2004년 3월에 발표되었다. 기존에 사용하던 GFSK 변좡식을 그대로 사용할 경우 1 Mbps의 전송속도를 유지하지만, Enhanced Data Rate(EDR)을 통해 최대 3 Mbps, 실제로는 2.1 Mbps의 속도가 지원된다. EDR은...","url": "http://localhost:4000/projects/bluetooth/generation/"
   },{
@@ -85,6 +88,9 @@ var store = [{
   },{
     "title": null,
     "excerpt":"","url": "http://localhost:4000/"
+  },{
+    "title": "KPI",
+    "excerpt":"","url": "http://localhost:4000/projects/kpi/"
   },{
     "title": "Bluetooth History",
     "excerpt":"블루투스 기기 여러 대를 동시에 연결할 수 있을까? 블루투스를 일상생활에서 항상 활용하는 나도 매번 헷갈려하는 문제다.   여러 대를 동시에 연결한다는 것은 point-to-point, 즉 일대일 연결을 말하는 것이다. Broadcast 방식의 단방향 전송은 해당하지 않는다.   기본적으로 여러 기기를 동시에 연결하기 위해서는 기기들이 보내는 패킷에 충돌이 일어나면 안된다.  ","url": "http://localhost:4000/projects/bluetooth/law/"
